4954f3a02aa758182cde2cbe583c80ebe0cb8fb1
Lawrince <Marklawrince730@gmail.com>
2025-12-16 17:10:26 +0800

    added crud for categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\CategoryResourceCollection;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::get();
+
+        return new CategoryResourceCollection($categories);
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        return new CategoryResource($category);
+    }
+
+    public function store(Request $request)
+    {
+        $category = Category::create($request->only([
+            'name',
+            'status'
+        ]));
+
+        return new CategoryResource($category);
+    }
+
+    public function update($id, Request $request)
+    {
+        $category = Category::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'status' => 'sometimes|string|max:255'
+        ]);
+
+        $category->update($validated);
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy($id)
+    {
+        // Find the user or fail with 404
+        $category = Category::findOrFail($id);
+
+        $category->delete();
+        return response()->json([
+            'message' => 'Category deleted successfully',
+            'id' => $id
+        ], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
@@ -11,5 +12,6 @@ Route::middleware('auth:sanctum')->group(function(){
 
     Route::apiResource('agents', AgentController::class);
     Route::get('/agent/profile', [AgentController::class, 'profile']);
+    Route::apiResource('categories', CategoryController::class);
 
 });
