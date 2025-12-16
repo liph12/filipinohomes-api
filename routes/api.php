0e0092591c314efade8983625f8ed611299be7ce
Lawrince <Marklawrince730@gmail.com>
2025-12-16 09:27:23 +0800

    created a route api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function(){
+
+    // // authenticated users only
+    // Route::get('/testing', function(){
+    //     return response()->json(['message' => 'API WORKS!']);
+    // });
+
+});
+
+
