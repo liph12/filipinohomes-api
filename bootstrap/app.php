030fa50af49e0cd25d38b7ceb5778cf1776fcab4
Lawrince <Marklawrince730@gmail.com>
2026-02-02 00:31:59 +0800

    made a json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->shouldRenderJsonWhen(
+        fn(Request $request) => $request->is('api/*') || $request->wantsJson()
+    );
     })->create();
