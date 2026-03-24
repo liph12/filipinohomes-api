145bed33c6f4e3522e50bca05ed9b5e3295d7ada
Lawrince <Marklawrince730@gmail.com>
2026-03-24 20:09:58 +0800

    added editor role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,7 +13,7 @@ class RoleSeeder extends Seeder
             ['id' => 1, 'name' => 'admin'],
             ['id' => 2, 'name' => 'agent'],
             ['id' => 3, 'name' => 'client'],
-            ['id' => 4, 'name' => 'super_admin'],
+            ['id' => 4, 'name' => 'editor'],
         ];
 
         DB::table('roles')->upsert($roles, ['id'], ['name']);
