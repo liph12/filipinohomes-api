3a3e98ab19c813cd7a22c3e29591ebeece7845a9
Philip Libres <libresphilip14@gmail.com>
2026-03-25 20:27:14 +0800

    added ai user search log

---
+++ app/Models/AiSearchLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AiSearchLog extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'user_info',
+        'country',
+        'month'
+    ];
+
+    protected $casts = [
+        'searches' => 'array'
+    ];
+}
