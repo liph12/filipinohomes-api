8f94e3d831de215a923995d24c5703fe85268456
Philip Libres <libresphilip14@gmail.com>
2026-03-30 11:41:31 +0800

    added rate limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(120)->by(
+                $request->user()?->id ?: $request->ip()
+            );
+        });
+    
+        // Auth (strict)
+        RateLimiter::for('auth', function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+    
+        // Chat (looser / bursty)
+        RateLimiter::for('chat', function (Request $request) {
+            return Limit::perMinute(300)->by($request->user()->id);
+        });
     }
 }
