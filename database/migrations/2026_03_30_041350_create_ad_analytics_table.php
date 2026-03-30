d26a1ecad4ce1c539048b4ed3282247050546631
Philip Libres <libresphilip14@gmail.com>
2026-03-30 13:25:37 +0800

    table migration ad_analytics added

---
+++ database/migrations/2026_03_30_041350_create_ad_analytics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ad_analytics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ad_id')->constrained()->cascadeOnDelete();
+            $table->string('country')->default('PH');
+            $table->string('state')->default('Manila');
+            $table->string('city')->default('Manila City');
+            $table->string('created_hour_at')->default(null);
+            $table->string('created_date_at')->default(null);
+            $table->unsignedBigInteger('impressions')->default(0);
+            $table->unsignedBigInteger('clicks')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ad_analytics');
+    }
+};
