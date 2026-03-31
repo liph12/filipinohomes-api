e1a8cc8210fa86d598e71052a7ec02e95140fc44
Philip Libres <libresphilip14@gmail.com>
2026-03-31 11:51:37 +0800

    user inquiry stored in database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\UserResource;
 use App\Services\User\LoginUserService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 use App\Mail\LoginOtpMailer;
 use App\Mail\InquiryMailer;
 use Illuminate\Support\Facades\Mail;
@@ -16,17 +17,49 @@ use App\Services\LeuterioreRealty\LrApiService;
 use App\Models\Agent;
 use Illuminate\Support\Facades\DB;
 use App\Models\UserInfo;
+use App\Models\Inquiry;
 
 class UserController extends Controller
 {
     public function sendInquiry(Request $request)
     {
+        $deviceId = $request->input('device_id');
+
+        if (!$deviceId) {
+            return response()->json([
+                'message' => 'Device ID is required.'
+            ], 400);
+        }
+
+        $cacheKey = "inquiry_attempts:{$deviceId}";
+        $banKey   = "inquiry_ban:{$deviceId}";
+
+        if (Cache::has($banKey)) {
+            return response()->json([
+                'message' => 'Too many requests. Try again after 5 minutes.'
+            ], 429);
+        }
+
+        $attempts = Cache::get($cacheKey, 0);
+
+        if ($attempts >= 5) {
+            Cache::put($banKey, true, now()->addMinutes(5));
+
+            return response()->json([
+                'message' => 'You are temporarily blocked for 5 minutes.'
+            ], 429);
+        }
+
+        Cache::put($cacheKey, $attempts + 1, now()->addMinute());
+
+        $userInfo = $request->input('user_info');
+
         $validated = $request->validate([
             'name'    => 'required|string|max:255',
             'email'   => 'required|email',
             'message' => 'required|string|max:5000',
         ]);
-            
+
         Mail::to('marklawrince730@gmail.com')->send(new InquiryMailer(
             $validated['name'],
             $validated['email'],
@@ -36,10 +69,21 @@ class UserController extends Controller
             ]
         ));
 
+        Inquiry::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'message' => $validated['message'],
+            'device' => $userInfo['device'],
+            'country' => $userInfo['country'],
+            'state' => $userInfo['state'],
+            'city' => $userInfo['city']
+        ]);
+
         return response()->json([
             'message' => 'Inquiry sent successfully!'
         ]);
     }
+    
     public function registerWithOtp(Request $request)
     {
         $validated = $request->validate([
