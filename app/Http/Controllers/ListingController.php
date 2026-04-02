8713c8b3159df6d69da8a75913e595f1fa73c6a8
Philip Libres <libresphilip14@gmail.com>
2026-04-02 16:22:36 +0800

    added resolvable listing(s) data by keyword or slug

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -94,21 +94,25 @@ class ListingController extends Controller
     }
 
     public function resolveByKeywordsAndSlug(Request $request)
-    {        
-        $listing = Listing::where('slug', $request->slug)->where('visibility', 'public')
-        ->with([
-            'property.propertyAttribute.subtype',
-            'property.nearbyFacility',
-            'category',
-            'agent' => function ($q) {
-                $q->withCount('listings');
-            }
-        ])
-        ->withCount([
-            'property as subtype_count' => function ($q) {
-                $q->whereHas('propertyAttribute.subtype');
-            }
-        ])->first();
+    {   
+        $listing = null;
+        if($slug = $request->input('slug'))
+        {
+            $listing = Listing::where('slug', $slug)->where('visibility', 'public')
+            ->with([
+                'property.propertyAttribute.subtype',
+                'property.nearbyFacility',
+                'category',
+                'agent' => function ($q) {
+                    $q->withCount('listings');
+                }
+            ])
+            ->withCount([
+                'property as subtype_count' => function ($q) {
+                    $q->whereHas('propertyAttribute.subtype');
+                }
+            ])->first();
+        }
 
         return [
             'property' => $listing,
