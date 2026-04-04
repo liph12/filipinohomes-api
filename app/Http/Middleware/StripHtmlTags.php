597b2fec8d798a28d90a0588fd784003ebc1adbc
Philip Libres <libresphilip14@gmail.com>
2026-04-04 18:48:42 +0800

    added cors to www.filipinohomes.com

---
+++ app/Http/Middleware/StripHtmlTags.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class StripHtmlTags
+{
+    /**
+     * Fields to exclude from stripping (e.g. rich-text editors, passwords).
+     */
+    protected array $except = [
+        'password',
+        'password_confirmation',
+    ];
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $input = $request->all();
+        array_walk_recursive($input, function (&$value, $key) {
+            if (! in_array($key, $this->except) && is_string($value)) {
+                $value = strip_tags($value);
+            }
+        });
+
+        $request->merge($input);
+
+        return $next($request);
+    }
+}
