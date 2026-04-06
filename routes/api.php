3a6e6c8b60cf8598abc04f58f67232ae24d162b9
johnmaizo <johnrobertmaizo@gmail.com>
2026-04-06 13:21:17 +0800

    eqwwqeewqeqw

--- routes/api.php
+++ routes/api.php
@@ -126,7 +126,7 @@ Route::middleware('strip.tags')->group(function(){
             Route::get('/my-listings', [ListingController::class, 'myListings']);
             Route::get('/all-listings', [ListingController::class, 'allListings']);
             Route::get('/user/dashboard', [ListingController::class, 'dashboard']);
-            Route::get('/user/status-by-date', [ListingController::class, 'dashboardStatusByDate']);
+            Route::get('/user/status-by-date', [ListingController::class,  'dashboardStatusByDate']);
             Route::get('/user/profile', [UserController::class, 'profile']);
             Route::get('/user/settings', [UserController::class, 'userSettings']);
             Route::post('agents', [AgentController::class, 'store']);
