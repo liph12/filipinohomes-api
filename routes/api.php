20a7d65b2d34678449d19091f53d5718bc6ad207
Lawrince <Marklawrince730@gmail.com>
2026-04-07 09:55:37 +0800

    added clicks and impression

--- routes/api.php
+++ routes/api.php
@@ -90,8 +90,8 @@ Route::middleware('strip.tags')->group(function(){
         Route::get('/page/agents/{slug}', [PageBuilderController::class, 'show']);
         Route::get('/page/agents', [PageBuilderController::class, 'index']);
         // PageBuilder public tracking
-        Route::post('/page/agents/{id}/impression', [PageBuilderController::class, 'trackImpression']);
-        Route::post('/page/agents/{id}/click', [PageBuilderController::class, 'trackClick']);
+        Route::post('/page/agents/{slug}/impression', [PageBuilderController::class, 'trackImpression']);
+        Route::post('/page/agents/{slug}/click', [PageBuilderController::class, 'trackClick']);
         Route::get('/offices/{slug}', [OfficeController::class, 'show']);
         Route::get('/__dev__/__admins__', [AgentController::class, 'admins']);
         Route::get('/resolve-properties-keywords', [ListingController::class, 'resolveByKeywordsAndSlug']);
